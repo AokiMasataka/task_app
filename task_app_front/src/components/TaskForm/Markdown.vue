c0a0b7b475c79298c1bb1240d7389e0c6c3f2fc4
AokiMasataka <4okimasataka@gmail.com>
2025-01-05 23:43:48 +0900

    feat: add marked

---
+++ task_app_front/src/components/TaskForm/Markdown.vue
@@ -0,0 +1,10 @@
+<template>
+    <div v-html="markdown"></div>
+</template>
+
+<script setup lang="ts">
+import { marked } from 'marked';
+
+const props = defineProps<{content: string}>();
+const markdown = marked(props.content);
+</script>
